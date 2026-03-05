f66290ee3a4da0afb1122be2750aea63ed49fdfb
chiendoan88 <hochientty2019@gmail.com>
2026-03-05 15:51:05 +0700

    Bai1a

---
+++ eclipse-workspace/Singleton_Pattern/src/Singleton/Pattern/PrintSpooler.java
@@ -0,0 +1,25 @@
+package Singleton.Pattern;
+
+public class PrintSpooler {
+
+    // biến lưu instance duy nhất
+    private static PrintSpooler instance;
+
+    // constructor private để không cho class khác tạo object
+    private PrintSpooler() {
+        init();
+    }
+
+    // phương thức khởi tạo
+    private void init() {
+        System.out.println("PrintSpooler initialized");
+    }
+
+    // phương thức lấy instance duy nhất
+    public static PrintSpooler getInstance() {
+        if (instance == null) {
+            instance = new PrintSpooler();
+        }
+        return instance;
+    }
+}
